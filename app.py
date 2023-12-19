7f9afeffbac6d2ef591df283972aab13122b3c60
Yacine Aslimi <yacineaslimi@gmail.com>
2023-12-19 08:04:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Basic Statistical Analysis App')
 
-data_input = st.text_area("Enter your data, separated by commas:")
+# Option for user to choose data input method
+data_option = st.selectbox("Choose your data input method", ["Enter Data", "Generate Fake Data"])
+
+data_input = ""
+if data_option == "Enter Data":
+    data_input = st.text_area("Enter your data, separated by commas:")
+elif data_option == "Generate Fake Data":
+    if st.button("Generate Data"):
+        data_input = ",".join([str(np.random.rand()) for _ in range(100)])
 
 if st.button('Analyze'):
     if data_input:
@@ -21,7 +30,16 @@ if st.button('Analyze'):
             st.write(f"Mean: {mean}")
             st.write(f"Median: {median}")
             st.write(f"Standard Deviation: {std_dev}")
+
+            # Create and display a simple plot
+            fig, ax = plt.subplots()
+            ax.hist(data, bins=20, color='blue', alpha=0.7)
+            ax.set_title('Data Distribution')
+            ax.set_xlabel('Values')
+            ax.set_ylabel('Frequency')
+            st.pyplot(fig)
+
         except ValueError:
             st.error("Please enter a valid list of numbers, separated by commas.")
     else:
-        st.warning("Please enter some data.")
+        st.warning("Please enter some data or generate fake data.")
